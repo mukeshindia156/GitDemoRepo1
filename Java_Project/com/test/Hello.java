45e599cfaef95ddf6075f2a1f4c463c56a21f3a0
mukeshindia156 <mukeshindia156@gmail.com>
2020-05-08 13:43:29 +0530

    updated Hello.java

--- Java_Project/com/test/Hello.java
+++ Java_Project/com/test/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 public static void main(String[] args){
 	for(int i=1; i<11;i++){
-		System.out.println("Hello Universe ..." + i);
+		System.out.println("Hello Universe from Java Program ..." + i);
 	}
 }
 
